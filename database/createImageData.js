79a9f3764a7773bb9c8fcb596143360e0b19cc53
Dustin Ancalade <umbi24@gmail.com>
2020-05-06 00:56:33 -0700

    created listing and image seed files

---
+++ database/createImageData.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const file = fs.createWriteStream('./imageSeedData.csv');
+const loremIpsum = require('lorem-ipsum').loremIpsum;
+
+const imageColumnNames = 'listing_id, image_url_1, image_url_2, image_url_3, image_url_4, image_url_5';
+
+file.write(imageColumnNames);
+
+//seedDB for listing_images
+for (let i = 0; i < 1e7; i ++) {
+  let listingId = 10001 + i;
+  let imageUrls = [];
+  for (let j = 0; j < 5; j++) {
+    let imageUrl = `https://picsum.photos/300/200?random=${listingId}${j}`;
+    imageUrls.push(imageUrl);
+
+  }
+  file.write(`\n ${listingId}, ${imageUrls[0]}, ${imageUrls[1]}, ${imageUrls[2]}, ${imageUrls[3]}, ${imageUrls[4]}`);
+}
+
+
+file.end();
--- database/createListingData.js
+++ database/createListingData.js
@@ -1,18 +1,16 @@
 const fs = require('fs');
-const file = fs.createWriteStream('./seedData.csv');
-const loremIpsum = require("lorem-ipsum").loremIpsum;
+const file = fs.createWriteStream('./listingSeedData.csv');
+const loremIpsum = require('lorem-ipsum').loremIpsum;
 
 const listingTypeOptions = ['Entire place', 'Private room', 'Hotel room', 'Shared room'];
 const listingCategoryOptions = ['apartment', 'house', 'hotel', 'cabin', 'bnb'];
 const listingColumnNames = 'listing_id, listing_type, listing_category, night_price, avg_review, num_review, number_beds, listing_title, is_fav';
 
-fs.writeFile('./seedData.csv', listingColumnNames, () => {
-  console.log('done');
-});
+file.write(listingColumnNames);
 
 // seedDB for listings table
-for (let i = 0; i < 1e6 + 2; i ++) {
-  let listingId = i;
+for (let i = 0; i < 100; i ++) {
+  let listingId = i + 10001;
   let listingType = listingTypeOptions[Math.floor(Math.random() * 4)];
   let listingCategory = listingCategoryOptions[Math.floor(Math.random() * 5)];
   let nightPrice = 100 + Math.random() * 100;
@@ -29,5 +27,4 @@ for (let i = 0; i < 1e6 + 2; i ++) {
   let isFav = i % 20 === 0 ? true : false;
   file.write(`\n ${listingId}, ${listingType}, ${listingCategory}, ${nightPrice}, ${avgReview}, ${numReview}, ${numberBeds}, ${listingTitle}, ${isFav}`);
 }
-
 file.end();

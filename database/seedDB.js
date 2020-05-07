c4e4d49c239a0c00262b4c3c80c1bc475d45c256
Dustin Ancalade <umbi24@gmail.com>
2020-05-06 21:04:42 -0700

    creates tables and relationships with sequelize for postgres  Please enter the commit message for your changes. Lines starting

--- database/seedDB.js
+++ database/seedDB.js
@@ -1,63 +1,52 @@
-//forgot that I had to create a branch. Please review the seedDB.js file. Thanks!
-const loremIpsum = require("lorem-ipsum").loremIpsum;
-const mysql = require('mysql');
-
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'recommendations'
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('dustinancalade', '', '', {
+  dialect: 'postgres'
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
 
-const listing_type_options = ['Entire place', 'Private room', 'Hotel room', 'Shared room'];
-const listing_category_options = ['apartment', 'house', 'hotel', 'cabin', 'bnb'];
+class Listing extends Model {}
+Listing.init({
+  listing_id: { DataTypes.INTEGER, unique: true, primaryKey: true, allowNull: false },
+  listing_type: { DataTypes.STRING, allowNull: false },
+  listing_category: {DataTypes.STRING, allowNull: false },
+  night_price: { DataTypes.FLOAT, allowNull: false },
+  avg_review: { DataTypes.FLOAT, allowNull: false },
+  num_review: { DataTypes.INTEGER, allowNull: false },
+  listing_title: { DataTypes.STRING, unique: true, allowNull: false },
+  is_fav: DataTypes.BOOLEAN,
+}, {sequelize, modelName: 'Listing'});
 
-//seedDB for listings table
-for (let i = 0; i < 100; i ++) {
-  let listing_id = 10001 + i;
-  let listing_type =  listing_type_options[Math.floor(Math.random() * 4)];
-  let listing_category = listing_category_options[Math.floor(Math.random() * 5)];
-  let night_price = 100 + Math.random() * 100;
-  let avg_review = 2 + Math.random() * 3;
-  let num_review = 2 + Math.floor(Math.random() * 50)
-  let number_beds = 1 + Math.floor(Math.random() * 7)
-  let listing_title = loremIpsum({
-    count: 1,
-    format:  "plain",
-    sentenceLowerBound: 3,
-    sentenceUpperBound: 6,
-    units: "sentences"
-  });
-  let is_fav = i % 20 === 0 ? true : false;
-  //INSERT INTO TABLE
-  let sql = `INSERT INTO listings
-  VALUES ("${listing_id}", "${listing_type}", "${listing_category}", ${night_price}, ${avg_review}, ${num_review}, ${number_beds}, "${listing_title}", ${is_fav});`
-  connection.query(sql,function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
+Listing.sync({force: true});
+// CREATE TABLE listings (
+//   listing_id INT NOT NULL ,
+//   listing_type VARCHAR(100) NOT NULL,
+//   listing_category VARCHAR(100) NOT NULL,
+//   night_price FLOAT NOT NULL,
+//   avg_review FLOAT NOT NULL,
+//   num_review INT NOT NULL,
+//   num_beds INT NOT NULL,
+//   listing_title VARCHAR(100) NOT NULL,
+//   is_fav BOOLEAN NOT NULL,
+//   PRIMARY KEY(listing_id)
+// );
 
-  });
-}
+class Listing_Image extends Model {}
+Listing_Image.init({
+  listing_id: {DataTypes.INTEGER, image_url},
+  image_url_1: {DataTypes.string, unique: true, allowNull: false},
+  image_url_2: {DataTypes.string, unique: true, allowNull: false},
+  image_url_3: {DataTypes.string, unique: true, allowNull: false},
+  image_url_4: {DataTypes.string, unique: true, allowNull: false},
+  image_url_5: {DataTypes.string, unique: true, allowNull: false}
+}, {sequelize, modelName: Listing_Image});
+ListingImage.sync({force: true});
 
-//seedDB for listing_images
-for (let i = 0; i < 100; i ++) {
-  let listing_id = 10001 + i;
-  for (let j = 0; j < 5; j++) {
-    let image_url = `https://picsum.photos/300/200?random=${listing_id}${j}`
-    let sql = `INSERT INTO listing_images
-    VALUES (${listing_id}, "${image_url}");`
-    connection.query(sql, function (err, result, fields) {
-      if (err) throw err;
-      console.log(result);
-    });
-  }
-}
+Listing.hasMany(Listing_Image, {foreignKey: 'listing_id'});
 
-
-
-
+// const imageColumnNames = 'listing_id, image_url_1, image_url_2, image_url_3, image_url_4, image_url_5';
+// CREATE TABLE listing_images (
+//   listing_id INT NOT NULL,
+//   image_url VARCHAR(100) NOT NULL,
+//   FOREIGN KEY (listing_id)
+//     REFERENCES listings(listing_id)
+// );

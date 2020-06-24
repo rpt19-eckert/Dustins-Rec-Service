9a4c70bca7fdf7a37212182e22e97b9d7f04453d
Dustin Ancalade <umbi24@gmail.com>
2020-06-23 20:44:37 -0700

    ready to send copies to extra instances

--- server/index.js
+++ server/index.js
@@ -13,11 +13,25 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.get('/loaderio-317b6cddee4e5fb2d20bdde3d2722c60', function (req, res) {
+  console.log('in request');
+  const options = {
+    root: path.join(__dirname, '../')
+  }
+  res.sendFile('loaderio-317b6cddee4e5fb2d20bdde3d2722c60.txt', options, (err) => {
+    if (err) {
+      console.log('err', err);
+    } else {
+      console.log('Sent');
+    }
+  });
+});
+
 app.get('/bundle.js', (req, res) => {
   if (req.header('Accept-Encoding').includes('br')) {
     res.set('Content-Encoding', 'br');
     res.set('Content-Type', 'application/javascript; charset=UTF-8');
-    // console.log('sent compressed file');
+    console.log('sent compressed file');
     return res.sendFile(path.join(__dirname, '../client/dist', '/bundle.js.br'));
   } else if (req.header('Accept-Encoding').includes('gz')) {
     res.set('Content-Encoding', 'gzip');
@@ -40,27 +54,13 @@ app.use(express.static(__dirname + '/../client/dist'));
 //   });
 // });
 
-app.get('/loaderio-317b6cddee4e5fb2d20bdde3d2722c60', function (req, res) {
-  console.log('in request');
-  const options = {
-    root: path.join(__dirname, '../')
-  }
-  res.sendFile('loaderio-317b6cddee4e5fb2d20bdde3d2722c60.txt', options, (err) => {
-    if (err) {
-      console.log('err', err);
-    } else {
-      console.log('Sent');
-    }
-  });
-
-});
 
 app.get('/listings/:id', function (req, res) {
   db.selectRelatedListings(req.params.id, function(err, data) {
     if (err) {
       res.sendStatus(500);
     } else {
-      // console.log('file sent');
+      console.log('file sent');
       res.json(data.rows);
       res.end();
     }
